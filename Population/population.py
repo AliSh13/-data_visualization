8e22a443a2d6cd05a0c2f40d4c21ad95a8c89b50
alex <alex-just2200@yandex.ru>
2019-07-25 14:06:07 +0300

    added sorted by population

--- Population/population.py
+++ Population/population.py
@@ -2,9 +2,10 @@ import csv
 import pygal.maps.world
 from pygal.maps.world import COUNTRIES
 
-file =  'data_csv.csv'
+file =  'Population/data_csv.csv'
 
 def get_countries_code(country_name):
+    '''находит код страны по ее названию'''
     for code, country in COUNTRIES.items():
         if country == country_name:
             return code
@@ -14,6 +15,7 @@ def get_countries_code(country_name):
 
 with open(file)  as f:
     pop_data = csv.reader(f)
+    #построение словаря с данными о коде страны и численности населения.
     countries_data = {}
     for pop in pop_data:
         if pop[2] == '2010':
@@ -21,13 +23,27 @@ with open(file)  as f:
             population = int(pop[3])
             code = get_countries_code(str(country_name))
             if code:
-                country_code[code] = population
+                countries_data[code] = population
+
+    # группировка стран по численности
+    cc_pops_1, cc_pops_2, cc_pops_3, cc_pops_4 = {}, {}, {}, {}
+    for cc, pop in  countries_data.items():
+        if pop < 1000000:
+            cc_pops_1[cc] = pop
+        elif pop < 100000000:
+            cc_pops_2[cc] = pop
+        elif pop < 1000000000:
+            cc_pops_3[cc] = pop
+        else:
+            cc_pops_4[cc] = pop
 
 
+worldmap = pygal.maps.world.World()
+worldmap.title = 'World population in 2010'
 
+worldmap.add('0-10m', cc_pops_1 )
+worldmap.add('10-100m', cc_pops_2 )
+worldmap.add('100-1bn', cc_pops_3 )
+worldmap.add('>1bn', cc_pops_4 )
 
-worldmap_chart = pygal.maps.world.World()
-worldmap_chart.title = 'World population in 2010'
-worldmap_chart.add('2010 countries', countries_data)
-
-worldmap_chart.render_to_file('world_population.svg')
+worldmap.render_to_file('Population/world_population.svg')
